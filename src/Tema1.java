553d411ac050f8700a796456fb83773aa90618ac
Nico Popa <nicopopa341@gmail.com>
2023-12-03 13:45:34 +0200

    Tema1

---
+++ src/Tema1.java
@@ -0,0 +1,99 @@
+//import java.util.Scanner;
+//
+//// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+//// then press Enter. You can now see whitespace characters in your code.
+//public class Tema1 {
+//    public static void main(String[] args) {
+//
+//        int x = -5;
+//        int y = 8;
+//        int z = 6;
+//
+//        System.out.println(x+y*z);
+//
+//        x=55;
+//        y=9;
+//        System.out.println((x+y) % y);
+//
+//        x=20;
+//        y= -3;
+//        z=5;
+//        int a = 8;
+//        System.out.println(x+y*z/a);
+//
+//        x = 5;
+//        y= 15;
+//        z=  3;
+//        a = 2;
+//        int b;
+//        b = 8;
+//        System.out.println(x+y/z*a-b%z);
+//
+//        System.out.println("Hello");
+//        System.out.println("Nicoleta");
+//
+//        a = 36;
+//        b = 6;
+//        System.out.println(a/b);
+//
+//        a = 8;
+//        b = 9;
+//        int c;
+//        int d;
+//        c = 5;
+//        d = 4;
+//        System.out.println(a*b-c/d);
+//
+//        a = 31;
+//        b = 3;
+//        c = 4;
+//        System.out.println((a%b)%c);
+//        System.out.println(a%(b%c));
+//
+//        a = 7;
+//        b = 1;
+//        c = 4;
+//        d = 3;
+//        x = 2;
+//        System.out.println(a%b+c/d*x-x%2);
+//
+//        int tc;
+//        int tf;
+//        tf = 86;
+//        tc = ((tf-32)*5)/9;
+//        System.out.println(tc);
+//
+//       a = 30;
+//       float bfraction = 0.0254f;
+//
+//        double raspuns_b = (a * bfraction);
+//        System.out.println(raspuns_b);
+//
+//        a = 2;
+//        int a_square;
+//        a_square = a * a;
+//        System.out.println(a_square);
+//        int a_cube;
+//        a_cube = a * a * a;
+//        System.out.println(a_cube);
+//        int a_fourth_power;
+//        a_fourth_power = a * a * a * a;
+//        System.out.println(a_fourth_power);
+//
+//        //int bmi;
+//        int kg;
+//        kg = 59;
+//
+//        float height = 1.59f;
+//
+//        float bmi = kg / (height * height);
+//        System.out.println(bmi);
+//
+//        Scanner input = new Scanner(System.in);
+//
+//
+//    }
+//
+//    }
+//
+//
